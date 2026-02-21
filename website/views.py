3b02f146945a0a45562df55ba53a3f35ad8fb884
Kamlesh Singh <kamsaa0001@gmail.com>
2026-02-21 13:18:10 +0530

    Fixed contact form and success message

--- website/views.py
+++ website/views.py
@@ -1,25 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project, Contact
 
 def home(request):
     projects = Project.objects.all().order_by('-created_at')
-    success = False
 
     if request.method == "POST":
         name = request.POST.get("name")
         email = request.POST.get("email")
         message = request.POST.get("message")
 
-        # Save message in database
+        # Save to database
         Contact.objects.create(
             name=name,
             email=email,
             message=message
         )
 
-        success = True  # Show success message on same page
+        return redirect("/?success=true")
 
-    return render(request, 'website/home.html', {
-        'projects': projects,
-        'success': success
-    })
+    return render(request, "website/home.html", {"projects": projects})
