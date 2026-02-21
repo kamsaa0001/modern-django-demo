b50855c39de70bf86c2be4f19a3be6207a773c0a
Kamlesh Singh <kamsaa0001@gmail.com>
2026-02-21 14:55:42 +0530

    Added email notification setup

--- modernsite/settings.py
+++ modernsite/settings.py
@@ -125,3 +125,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kamsaa0001@gmail.com'
+EMAIL_HOST_PASSWORD = 'ujgumhwtlwhlmwkf'
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Project, Contact
+from django.core.mail import send_mail
 
 def home(request):
     projects = Project.objects.all().order_by('-created_at')
@@ -14,6 +15,12 @@ def home(request):
             name=name,
             email=email,
             message=message
+        )
+        send_mail(
+            subject="New Client Message from Website",
+            message=f"Name: {name}\nEmail: {email}\nMessage: {message}",
+            from_email=None,
+           recipient_list=["kamsaa0001@gmail.com"],
         )
 
         return redirect("/?success=true")
